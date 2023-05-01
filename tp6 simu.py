20f5dd159bf9439b961422bcc90d430fefcdbe52
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-04-30 22:42:41 -0300

    histograma a pdf

--- tp6 simu.py
+++ tp6 simu.py
@@ -1,26 +1,50 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+from scipy import stats
 
-def fdp(archivo, bins):
+def fdp(archivo):
     data_frame = pd.read_excel(archivo)
-    datos = data_frame['Datos'].tolist()
-    return np.histogram(datos, bins=bins, density=True)
+    datos = data_frame['Datos'].values
+    return stats.gaussian_kde(datos)
 
-def valor_segun_fdp(bin_edges, counts):
-    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
-    return np.random.choice(bin_centers, p=counts/counts.sum())
 
-counts_turnos, bin_edges_turnos = fdp('intervalos entre turnos.xlsx', 40)
-counts_comandos, bin_edges_comandos = fdp('cantidad de comandos.xlsx', 10)
-count_duracion, bin_edges_duracion = fdp('duracion de comandos.xlsx', 20)
+def valor_segun_fdp(kde):
+    return kde.resample(1)[0][0]
+
+fdp_intervalo_turnos= fdp('intervalos entre turnos.xlsx')
+fdp_cantidad_comandos = fdp('cantidad de comandos.xlsx')
+fdp_duracion_comando = fdp('duracion de comandos.xlsx')
 
 
 def nuevo_intervalo_entre_turnos():
-    return valor_segun_fdp(bin_edges_turnos, counts_turnos)
+    return valor_segun_fdp(fdp_intervalo_turnos)
 
 def nueva_cantidad_de_comandos():
-    return valor_segun_fdp(bin_edges_comandos, counts_comandos)
+    return int(valor_segun_fdp(fdp_cantidad_comandos))
 
 def nueva_duracion_de_comando():
-    return valor_segun_fdp(bin_edges_duracion, count_duracion)
+    return valor_segun_fdp(fdp_duracion_comando)
+
+print(nueva_cantidad_de_comandos())
+print(nueva_cantidad_de_comandos())
+print(nueva_cantidad_de_comandos())
+
+valores = []
+
+for i in range(999):
+    valores.append(nuevo_intervalo_entre_turnos())
+
+print ("Intervalo entre turnos:", sum(valores) / len(valores))
+
+valores = []
+
+for i in range(999):
+    valores.append(nueva_cantidad_de_comandos())
+
+print ("Cantidad de comandos:", sum(valores) / len(valores))
+
+valores = []
+
+for i in range(999):
+    valores.append(nueva_duracion_de_comando())
+
+print ("Duracion de comandos:", sum(valores) / len(valores))
